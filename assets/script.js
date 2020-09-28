9a02359caf6b6a6a5a1dc61b83b062b2887d8acc
patinoman <68838876+patinoman@users.noreply.github.com>
2020-09-28 01:35:14 -0700

    added styling/js to portfolio

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+document.getElementById("air-hiker").onclick = function () {
+  location.href = "https://yalitzab.github.io/air-hiker/";
+};
+document.getElementById("password-generator").onclick = function () {
+  location.href = "https://patinoman.github.io/password-generator/";
+};
+document.getElementById("work-day-planner").onclick = function () {
+  location.href = "https://patinoman.github.io/work-day-planner/";
+};
+document.getElementById("weather-dashboard").onclick = function () {
+  location.href = "https://patinoman.github.io/weather-dashboard/";
+};
+document.getElementById("run-buddy").onclick = function () {
+  location.href = "https://patinoman.github.io/run-buddy/";
+};
